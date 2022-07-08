02e259d66b9d2364ce820e0dbc120c553ebcebd2
FredyBarrantes <fredybarrantesg@gmail.com>
2022-07-08 15:43:00 -0500

    Added comments and translation to :GuessNumber.py: and :pcGuessNumber.py:

--- miniGames/GuessNumber.py
+++ miniGames/GuessNumber.py
@@ -8,20 +8,31 @@ import random
 
 ale_num = random.randint(1, 10)
 while(True):
+    print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
     print("Digite un numero entero entre 1 y 10\nType an integer number between 1 and 10")
     try:
         num_usr = int(input(": "))
+        print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
         if num_usr == 0:
             raise ZeroDivisionError
     except ValueError:
+        print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
         print("Por favor ingrese solo numeros.")
         print("Please type just numbers.")
+        print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
     except ZeroDivisionError:
         break
     if num_usr > ale_num:
-        print("El numero ingresado es mayor")
+        print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
+        print("El numero",num_usr,"es mayor\nNumber",num_usr,"is largest")
+        print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
     elif num_usr < ale_num:
-        print("El numero ingresado es menor")
+        print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
+        print("El numero",num_usr,"es menor\nNumber",num_usr,"is lower")
+        print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
     else:
+        print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
         print("el numero aleatorio es",ale_num,"Adivinaste!!!")
+        print("The random number is",ale_num,"you've guessed!!!")
+        print("////-////-////-////-////-////-////-////-////-////-////-////-////-////-////-////")
         break
